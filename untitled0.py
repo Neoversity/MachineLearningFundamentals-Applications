76fe02dbcf65692a0d9bddcf857c42ff59292eec
ANTON BABENKO <antonbabenko1983@gmail.com>
2024-11-23 23:24:08 +0200

    test

--- untitled0.py
+++ untitled0.py
@@ -11,13 +11,14 @@ Created on Fri Nov 22 00:52:38 2024
 # print(torch.cuda.get_device_name(0))  # Має повернути 'NVIDIA GeForce RTX 3080 Ti'
 
 import tensorflow as tf
-import torch
+#import torch
+#print("TensorFlow version:", tf.__version__)
+#print("Built with CUDA support:", tf.test.is_built_with_cuda())
+#print("GPU Available:", tf.config.list_physical_devices('GPU'))
+
+
 print("TensorFlow version:", tf.__version__)
 print("Built with CUDA support:", tf.test.is_built_with_cuda())
-print("GPU Available:", tf.config.list_physical_devices('GPU'))
-
-
-print("GPU доступний:", torch.cuda.is_available())
-print("Назва GPU:", torch.cuda.get_device_name(0))
+print("Available GPUs:", tf.config.list_physical_devices('GPU'))
 
 
